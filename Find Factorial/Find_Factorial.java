502734e965455cae8b9e6f1a1efe63ec9c2921c8
IAMMRJATIN <jatin.sharma7281@gmail.com>
2024-10-06 00:02:15 +0530

    Java program for find the Factorial of an input number

---
+++ Find Factorial/Find_Factorial.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Find_Factorial {
+    public static void main(String[] args) {
+        // int num = 5, factorial = 1;
+        // for( int i = 1; i <= num; i++){
+        //     factorial *= i;
+        // }
+        // System.out.println(factorial);
+
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter a number who's factorial you want to print : ");
+        int num = obj.nextInt();
+
+        int i, factorial = 1;
+        for ( i = 1; i <= num; i++){
+            factorial *= i;
+        }
+        System.out.println("Factorial of the number " + num + " is : " + factorial);
+    }
+}
